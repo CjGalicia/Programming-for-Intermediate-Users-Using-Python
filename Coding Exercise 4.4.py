6f9a37e2b880b20060b02d7d54ee4319248a5828
CjGalicia <carljeremygalicia@gmail.com>
2022-05-26 17:23:33 +0800

    Commit for coding exercise 4.4

---
+++ Coding Exercise 4.4.py
@@ -0,0 +1,6 @@
+from functionfile import add
+from functionfile import come_from
+
+add(1, 5)
+come_from("Quezon City")
+
---
+++ functionfile.py
@@ -0,0 +1,14 @@
+def greeting():
+    print("Good day adventurer")
+
+def come_from(land):
+    print("Hello im from" + land)
+
+def add(x, y):
+    print(x+y)
+
+def difference(x, y):
+    print(x-y)
+
+def product(x, y):
+    print(x*y)
