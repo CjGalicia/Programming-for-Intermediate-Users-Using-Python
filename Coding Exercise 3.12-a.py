62f2646f3496c8c2f75c80d3af53fd6f77adb603
CjGalicia <carljeremygalicia@gmail.com>
2022-05-26 16:57:35 +0800

    Commit for coding exercise 3.12

---
+++ Coding Exercise 3.12-a.py
@@ -0,0 +1,3 @@
+x = 500
+if x > 100:
+    raise Exception("This code will result in an error if x has bigger than 100.")
---
+++ Coding Exercise 3.12-e.py
@@ -0,0 +1,5 @@
+best_burger = "Burger King"
+number_2_burger = "McDonald's"
+
+assert best_burger == "Burger King"
+assert number_2_burger == "McDonalds's"
